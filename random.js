91628420ed16928f512f07b569dd3aa7e0e7ac37
davidLiu <david-khala@hotmail.com>
2019-01-02 16:06:02 +0800

    add random string

--- random.js
+++ random.js
@@ -1,9 +1,12 @@
+const crypto = require('crypto');
 exports.randomKeyOf = (obj) => {
 	const keys = Object.keys(obj);
 	const keyIndex = Math.floor(Math.random() * keys.length);
 	return keys[keyIndex];
 };
-const crypto = require('crypto');
 exports.randomHex = (length) => {
 	return crypto.randomBytes(length).toString('hex');
+};
+exports.randomString = (length) => {
+	return [...Array(length)].map(() => (~~(Math.random() * 36)).toString(36)).join('');
 };
--- test/random.js
+++ test/random.js
@@ -1,4 +1,6 @@
-const {randomHex} = require('../random');
+const {randomHex, randomString} = require('../random');
 const logger = require('../logger').new('test:random');
 const hex = randomHex(20);
 logger.debug(hex);
+const randStr = randomString(16);
+logger.debug(randStr);
