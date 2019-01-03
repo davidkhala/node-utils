2002db7a7203c407c7c2644ac2aff657785c30a7
davidliu <david-khala@hotmail.com>
2019-01-03 18:19:56 +0800

    buffer test: not simple way to handle JSON

--- test/syntax.js
+++ test/syntax.js
@@ -1,11 +1,26 @@
-class TestClass {
-	async p() {
-		return new Promise(resolve => {
-			console.debug('this', this);
-			resolve();
-		});
+const testArrowFunction = async () => {
+	class TestClass {
+		async p() {
+			return new Promise(resolve => {
+				console.debug('this', this);
+				resolve();
+			});
+		}
 	}
-}
 
-const testObj = new TestClass();
-testObj.p();
+	const testObj = new TestClass();
+	await testObj.p();
+};
+const bufferTest = (obj) => {
+	const bytes = Buffer.from(JSON.stringify(obj));
+	console.log(bytes.toString());
+};
+
+
+const tests = async () => {
+	await testArrowFunction();
+	bufferTest({david: 'liu'});
+	bufferTest('liu');
+
+};
+tests();
