03170d94fe610cf6d77c45a8032fd82e5f6b72e5
davidliu <david-khala@hotmail.com>
2019-01-03 14:16:18 +0800

    missing commit:log levels enhance npm ignore add

--- logger.js
+++ logger.js
@@ -1,14 +1,25 @@
 const winston = require('winston');
+const levels = [
+	'error',
+	'warn',
+	'info',
+	'verbose',
+	'debug',
+	'silly'
+];
 /**
  *
- * @param moduleName
- * @returns {*} console logger
+ * @param {string} moduleName
+ * @param {number} level default 4:'debug'
+ * @return {winston.LoggerInstance}
  */
-exports.new = (moduleName) => {
+exports.new = (moduleName, level = 4) => {
+	//If you do not explicitly define the levels that winston should use the npm levels.https://www.npmjs.com/package/winston#logging-levels
+
 	return new (winston.Logger)({
 		transports: [
 			new (winston.transports.Console)({
-				level: 'debug',
+				level: levels[level],
 				colorize: true,
 				label: moduleName,
 				timestamp: true,
@@ -16,12 +27,19 @@ exports.new = (moduleName) => {
 		]
 	});
 };
-exports.newFile = (moduleName, logFile) => {
+/**
+ *
+ * @param {string} moduleName
+ * @param {string} logFile
+ * @param {number} level default 4:'debug'
+ * @return {winston.LoggerInstance}
+ */
+exports.newFile = (moduleName, logFile, level = 4) => {
 	return new (winston.Logger)({
 		transports: [
 			new (winston.transports.File)({
 				//property name: https://github.com/winstonjs/winston/tree/2.x#multiple-transports-of-the-same-type
-				level: 'debug',
+				level: levels[level],
 				label: moduleName,
 				filename: logFile,
 				timestamp: true,
