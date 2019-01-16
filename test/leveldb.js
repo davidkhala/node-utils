f73c6c476d349117cadea8f5527fedbbb3e0f495
davidLiu <david-khala@hotmail.com>
2019-01-16 18:38:42 +0800

    prepare for patch

--- test/leveldb.js
+++ test/leveldb.js
@@ -3,8 +3,9 @@ const LevelDB = require('../leveldb');
 
 const transientStore = '/home/mediconcen/Documents/backupVolumes/peer0.delphi/transientStore';
 const histroy = '/home/mediconcen/Documents/backupVolumes/peer0.delphi/ledgersData/historyLeveldb';
+const index = '/home/mediconcen/Documents/backupVolumes/peer0.delphi/ledgersData/chains/index';
 const path = histroy;
-const levelconn = new LevelDB(path);
+const levelconn = new LevelDB(index);
 
 const flow = async () => {
 	await levelconn.connect();
