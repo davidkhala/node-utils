ade39b166b02281b6128c3f507a3baa6acdcfc0e
davidLiu <david-khala@hotmail.com>
2019-04-10 07:18:00 +0800

    missing commit message: add execResponsePrint

--- logger/test/index.js
+++ logger/test/index.js
@@ -1,4 +1,8 @@
 const Logger = require('../');
-const label = "test";
+const label = 'test';
 const logger = Logger.new(label);
+const path = require('path');
+const filePath = path.resolve('test.Log');
+const fileLogger = Logger.newFile('test:file', filePath);
 logger.debug('abc');
+fileLogger.error('abc', 'cde');
