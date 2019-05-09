b84fa461a37c8675294dc0aa47dbd19c4b03b878
davidkhala <david-khala@hotmail.com>
2019-05-10 00:42:13 +0800

    change test bucketName

--- aws/test/S3.js
+++ aws/test/S3.js
@@ -1,6 +1,6 @@
 const region = 'ap-southeast-1';
 const bucket_name = 'mcc-lgstorage';
-const newBucketName = 'test-S3'.toLowerCase();
+const newBucketName = 'mcc-test-S3'.toLowerCase();
 const S3 = require('../S3');
 
 const s3 = new S3(region);
@@ -11,7 +11,7 @@ const task = async () => {
 	await s3.createBucket(newBucketName);
 	buckets = await s3.listBuckets();
 	console.info('list buckets', buckets);
-	await s3.deleteBucket(newBucketName)
+	await s3.deleteBucket(newBucketName);
 	buckets = await s3.listBuckets();
 	console.info('list buckets', buckets);
 };
