37e9e0774c33fd271bf48dddc3b0fbf7b503c618
davidLiu <david-khala@hotmail.com>
2019-05-09 21:30:02 +0800

    list buckets

--- aws/test/S3.js
+++ aws/test/S3.js
@@ -4,6 +4,7 @@ const S3 = require('../S3');
 
 const s3 = new S3(region);
 const task = async () => {
-	await s3.listBuckets();
+	const buckets = await s3.listBuckets();
+	console.info('list buckets', buckets);
 };
 task();
