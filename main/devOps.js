b2436c2d869b3c9d2f92b3b7ad0476dde3edddf2
davidkhala <david-khala@hotmail.com>
2019-07-20 12:14:32 +0800

    0.5.7 add isPortInUse

--- main/devOps.js
+++ main/devOps.js
@@ -1,5 +1,66 @@
-exports.localhost = '127.0.0.1';
+const localhost = '127.0.0.1';
+exports.localhost = localhost;
 const os = require('os');
 exports.hostname = os.hostname;
 exports.tempdir = os.tmpdir();
 exports.homedir = os.homedir();
+
+class NetworkSocket {
+	constructor(port, host) {
+		const {Socket} = require('net');
+		this.socket = new Socket();
+		this.port = port;
+		this.host = host;
+		this.listeners = {
+			connect: undefined,
+			timeout: undefined,
+			error: undefined,
+			close: undefined
+		};
+
+	}
+
+	connect(timeout) {
+		this.socket.setTimeout(timeout);
+		this.socket.connect(this.port, this.host);
+	}
+
+	close() {
+		this.socket.destroy();
+	}
+
+	setExclusiveListener(eventType, listener) {
+		if (this.listeners[eventType]) {
+			this.socket.removeListener(eventType, this.listeners[eventType]);
+		}
+		this.socket.on(eventType, listener);
+		this.listeners[eventType] = listener;
+	}
+}
+
+exports.isPortInUse = async (port, host = localhost, timeout = 1000) => {
+	const socket = new NetworkSocket(port, host);
+	return new Promise((resolve, reject) => {
+		const onConnect = () => {
+			socket.close();
+			resolve(true);
+		};
+		const onTimeout = () => {
+			socket.close();
+			resolve(false);
+		};
+		const onError = (err) => {
+			socket.close();
+			if (err.code === 'ECONNREFUSED') {
+				resolve(false);
+			} else {
+				reject(err);
+			}
+		};
+		socket.setExclusiveListener('connect', onConnect);
+		socket.setExclusiveListener('timeout', onTimeout);
+		socket.setExclusiveListener('error', onError);
+		socket.connect(timeout);
+	});
+
+};
