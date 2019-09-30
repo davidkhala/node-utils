27d11d325db55e34d70f590c4128c42359c58436
davidLiu <david-khala@hotmail.com>
2019-09-30 13:07:08 +0800

    sqs. destroy test

--- aws/test/sqs.js
+++ aws/test/sqs.js
@@ -4,11 +4,13 @@ const sqs = new SQS(region);
 const queue = 'topicA';
 const task = async () => {
 	await sqs.create(queue);
-	const queues = await sqs.list();
+	let queues = await sqs.list();
 	console.debug(queues);
 
 	let destroyResult = await sqs.destroy(queues[0]);
 	console.debug({destroyResult});
+	queues = await sqs.list();
+	console.debug(queues);
 };
 task();
 
