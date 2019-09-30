21b28052d8136009b570dabee38783f5277b8bc6
davidLiu <david-khala@hotmail.com>
2019-09-30 13:12:40 +0800

    purge interval limit: 60s

--- aws/test/sqs.js
+++ aws/test/sqs.js
@@ -2,6 +2,9 @@ const {SQS} = require('../sqs');
 const region = 'ap-southeast-1';
 const sqs = new SQS(region);
 const queue = 'topicA';
+const sleep = async (ms) => {
+	return new Promise(resolve => setTimeout(resolve, ms));
+};
 const task = async () => {
 	await sqs.create(queue);
 	let queues = await sqs.list();
@@ -10,7 +13,10 @@ const task = async () => {
 	let destroyResult = await sqs.destroy(queues[0]);
 	console.debug({destroyResult});
 	queues = await sqs.list();
-	console.debug(queues);
+	console.debug('immediate after deletion', queues);
+	await sleep(60000);
+	queues = await sqs.list();
+	console.debug('60 seconds after deletion', queues);
 };
 task();
 
