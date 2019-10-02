6286b04ded8e5583f74a0ca91e4ebc5d7c3ab7ac
davidLiu <david-khala@hotmail.com>
2019-10-02 17:16:02 +0800

    [DONE] test with bitnami/rabbitmq

--- rabbitmq/test/helper.sh
+++ rabbitmq/test/helper.sh
@@ -1,2 +1,9 @@
-
-curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-rabbitmq/master/docker-compose.yml > docker-compose.yml
+set -e
+getDockerComposeFile() {
+  curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-rabbitmq/master/docker-compose.yml >docker-compose.yml
+}
+createVolume() {
+  mkdir volume
+  sudo chmod 777 volume
+}
+$1
