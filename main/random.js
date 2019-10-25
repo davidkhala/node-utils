fd239b4956be18380e26a46a74a8b422ce7b83a8
david <david-khala@hotmail.com>
2019-10-25 10:07:17 +0800

    move in to reduce import size

--- main/random.js
+++ main/random.js
@@ -1,10 +1,10 @@
-const crypto = require('crypto');
 exports.randomKeyOf = (obj) => {
 	const keys = Object.keys(obj);
 	const keyIndex = Math.floor(Math.random() * keys.length);
 	return keys[keyIndex];
 };
 exports.randomHex = (length) => {
+	const crypto = require('crypto');
 	return crypto.randomBytes(length).toString('hex');
 };
 /**
