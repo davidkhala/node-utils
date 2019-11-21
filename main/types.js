dea1d7116cbcfcea05c214b3992edeaa77bc5fea
david <david-khala@hotmail.com>
2019-11-21 21:22:36 +0800

    add types

---
+++ main/types.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef {number} integer
+ */
+/**
+ * @typedef {string} intString '0'
+ */
+/**
+ * @typedef {string} hexString '58ab'
+ */
+/**
+ * @typedef {string} CertificatePem certificate containing the public key in PEM format.
+ */
