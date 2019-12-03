bb5f5654d4f8ee0483cd5c3712121d326034a0ea
david <david-khala@hotmail.com>
2019-12-03 18:50:40 +0800

    as install&Run

--- aws/install.sh
+++ aws/install.sh
@@ -1,8 +1,11 @@
 set -e
 
 xRayDaemon() {
-	wget https://s3.dualstack.us-east-2.amazonaws.com/aws-xray-assets.us-east-2/xray-daemon/aws-xray-daemon-3.x.deb
-	sudo apt install ./aws-xray-daemon-3.x.deb
-	rm aws-xray-daemon-3.x.deb
+	if ! xray --version; then
+		wget https://s3.dualstack.us-east-2.amazonaws.com/aws-xray-assets.us-east-2/xray-daemon/aws-xray-daemon-3.x.deb
+		sudo apt install ./aws-xray-daemon-3.x.deb
+		rm aws-xray-daemon-3.x.deb
+	fi
+	xray
 }
 $1
