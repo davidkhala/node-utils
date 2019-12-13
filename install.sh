c78ff90c09c3cbb47ec09305d56b419a1469d5dc
Yuxiang LIU <david-khala@hotmail.com>
2019-12-13 12:49:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,6 +38,9 @@ typeScript() {
 nodeVersionManager() {
 	if ! nvm --version; then
 		curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+		export NVM_DIR="$HOME/.nvm"
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 	fi
 }
 pm2() {
