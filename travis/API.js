e86d657101e8cfef15840e3f8d05b982f92d848a
david <david-khala@hotmail.com>
2020-04-15 16:17:21 +0800

    [DEBUG] HSM fix

--- travis/API.js
+++ travis/API.js
@@ -1,14 +1,12 @@
-// https://docs.travis-ci.com/user/running-build-in-debug-mode/#restarting-a-job-in-debug-mode-via-api
-
-// curl -s -X POST \
-//   -H "Content-Type: application/json" \
-//   -H "Accept: application/json" \
-//   -H "Travis-API-Version: 3" \
-//   -H "Authorization: token ********************" \
-//   -d "{\"quiet\": true}" \
-//   https://api.travis-ci.com/job/${id}/debug
 const {axiosPromise} = require('khala-axios');
-const debugBuild = async () => {
+
+/**
+ * https://docs.travis-ci.com/user/running-build-in-debug-mode/#restarting-a-job-in-debug-mode-via-api
+ * @param token API token from [your profile page](https://travis-ci.com/profile)
+ * @param id Job id, got from under 'Job log -> Build system information'
+ * @return {Promise<void>}
+ */
+const debugBuild = async (token, id) => {
 	const headers = {
 		'Content-Type': 'application/json',
 		Accept: 'application/json',
@@ -18,5 +16,7 @@ const debugBuild = async () => {
 	const body = {quiet: true};
 	const url = `https://api.travis-ci.com/job/${id}/debug`;
 	await axiosPromise({url, body}, {headers});
-  
 };
+debugBuild(process.env.token, process.env.jobId).catch(err => {
+	console.error(err.response.data);
+});
