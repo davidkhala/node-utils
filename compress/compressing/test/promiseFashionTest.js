c2a2429bdd068e81f954a6968c0e39daa2b9f606
david <david-khala@hotmail.com>
2020-05-17 18:07:17 +0800

    [WIP] compressing

--- compress/compressing/test/promiseFashionTest.js
+++ compress/compressing/test/promiseFashionTest.js
@@ -1,13 +1,21 @@
-const Compress = require('../index');
-const compress = new Compress();
-const source = Buffer.from('abc');
-const dest = 'abc';
+const compress = require('compressing');
+
+const path = require('path');
+
 const task = async () => {
 	switch (parseInt(process.env.taskID)) {
-		case 0:
-			await compress.compressFile(source, dest);
+		case 0: {
+			// taskID=0 node promiseFashionTest.js
+			const src = path.resolve(__dirname, 'material');
+			const dest = 'dir.tar.gz';
+			await compress.tar.compressDir(src, dest);
+		}
 			break;
-		case 1:
+		case 1: {
+			const src = path.resolve(__dirname, 'dir.tar.gz');
+			const dest = path.resolve(__dirname);
+			await compress.tar.uncompress(src, dest);
+		}
 			break;
 	}
 
