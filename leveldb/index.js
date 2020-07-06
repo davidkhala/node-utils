8f97fc898ce0251718b4c7cacb8c6bde686bdec9
david <david-khala@hotmail.com>
2020-07-06 20:20:09 +0800

    [WIP] mocha test migration update leveldb.js

--- leveldb/index.js
+++ leveldb/index.js
@@ -19,9 +19,6 @@ class LevelDB extends DBInterface {
 		return this.connection.put(key, value);
 	}
 
-	isOpen() {
-		return this.connection.isOpen();
-	}
 
 	async disconnect() {
 		await this.connection.close();
@@ -31,27 +28,45 @@ class LevelDB extends DBInterface {
 		return this.connection.get(key);
 	}
 
-	async list(opts, onClose) {
-		opts = LevelDB.optionsBuilder(opts);
-		if (typeof onClose !== 'function') {
-			onClose = () => {
-			};
-		}
+	static async _next(iterator) {
 		return new Promise((resolve, reject) => {
-			const results = [];
-			this.connection.createReadStream(opts)
-				.on('data', ({key, value}) => {
-					results.push({key, value});
-				})
-				.on('error', (err) => {
-					reject(err);
-				})
-				.on('close', onClose)
-				.on('end', () => {
-					resolve(results);
-				});
+			iterator.next((err, key, value) => {
+				if (err) {
+					return reject(err);
+				}
+				resolve({key, value});
+			});
 		});
+	}
 
+	static async _end(iterator) {
+		return new Promise((resolve, reject) => {
+			iterator.end((err) => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	}
+
+	async list(opts) {
+
+		const iterator = this.connection.iterator(opts);
+		const results = [];
+		const getNext = async () => {
+			const {key, value} = await LevelDB._next(iterator);
+			if (key) {
+				results.push({key, value});
+				await getNext();
+			}
+		};
+		try {
+			await getNext();
+			return results;
+		} finally {
+			await LevelDB._end(iterator);
+		}
 	}
 
 	static optionsBuilder({gt, gte, lt, lte, reverse, limit, keys, values} = {
