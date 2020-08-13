46889af23826125d6c6c7acec177bbc237a13a5a
davidLiu <david-khala@hotmail.com>
2020-08-13 14:00:41 +0800

    trying with winston-json-formatter

--- logger/winston3/test/index.js
+++ logger/winston3/test/index.js
@@ -1,3 +1,23 @@
 const {new: newLogger} = require('../');
-const logger = newLogger('b');
-logger.debug({a: {a: 'b'}});
+describe('Winston3: console', () => {
+
+	it('smoke', () => {
+		const logger = newLogger('b');
+		logger.debug({a: {a: 'b'}});
+	});
+	it('with `winston-json-formatter`', () => {
+		const logger = newLogger('b');
+		const {configuredFormatter} = require('winston-json-formatter');
+		const options = {
+			typeFormat: 'console',
+			service: 'service name',
+			version: 'version name'
+		};
+		logger.format = configuredFormatter(options); // overwriting
+		logger.info('string');
+		logger.info({json: 'json'}); // No logger label support
+
+	});
+});
+
+
