dc6d52d48626f4c8beeef53493fc52e6259cb0ee
DavidLiu <david.yx.liu@oracle.com>
2021-08-05 23:27:42 +0800

    Fix levelTest
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

--- leveldb/test/levelTest.js
+++ leveldb/test/levelTest.js
@@ -6,14 +6,14 @@ const logger = require('khala-logger/log4js').consoleLogger('test:leveldb');
 describe('Leveldb reader', () => {
 
 	it('histroy', async () => {
-		const histroy = path.resolve(__dirname, 'artifacts/ledgersData/historyLeveldb');
+		const histroy = path.resolve(__dirname, 'fixtures/ledgersData/historyLeveldb');
 		const levelconn = new LevelDB(histroy);
 		await levelconn.connect();
 		const aValue = await levelconn.list();
 		logger.info(aValue);
 	});
 	it('index', async () => {
-		const index = path.resolve(__dirname, 'artifacts/ledgersData/chains/index');
+		const index = path.resolve(__dirname, 'fixtures/ledgersData/chains/index');
 		const levelconn = new LevelDB(index);
 		await levelconn.connect();
 		const aValue = await levelconn.list();
