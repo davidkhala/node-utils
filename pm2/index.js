f1658c8685ae60cf9aa58186084d040c9323f1fd
DavidLiu <david.yx.liu@oracle.com>
2021-08-20 11:41:13 +0800

    khala-pm2 @ 0.1.0
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

--- pm2/index.js
+++ pm2/index.js
@@ -1,7 +1,8 @@
 const pm2 = require('pm2');
 const logger = require('khala-logger/log4js').consoleLogger('pm2 Manager');
 const fs = require('fs');
-exports.PM2 = class {
+
+class PM2 {
 	constructor() {
 
 	}
@@ -9,7 +10,9 @@ exports.PM2 = class {
 	async connect() {
 		await new Promise((resolve, reject) => {
 			pm2.connect(err => {
-				if (err) reject(err);
+				if (err) {
+					reject(err);
+				}
 				resolve();
 			});
 		});
@@ -25,7 +28,9 @@ exports.PM2 = class {
 		if (process) {
 			logger.warn(`process ${name} exist`);
 		} else {
-			if (!fs.existsSync(script)) throw `invalid script path ${script}`;
+			if (!fs.existsSync(script)) {
+				throw Error(`invalid script path ${script}`);
+			}
 			process = await new Promise((resolve, reject) => {
 				pm2.start({name, script, env}, (err, process) => {
 					if (err) {
@@ -45,14 +50,16 @@ exports.PM2 = class {
 		return await this.run({name, script, env});
 	}
 
-	async delete({name}) {
+	async delete(name) {
 		const process = await this.get({name});
 		if (!process) {
 			logger.warn(`process ${name} not exist, delete skipped`);
 		} else {
 			await new Promise((resolve, reject) => {
 				pm2.delete(name, (err) => {
-					if (err) reject(err);
+					if (err) {
+						reject(err);
+					}
 					resolve();
 				});
 			});
@@ -64,14 +71,20 @@ exports.PM2 = class {
 	async list() {
 		return await new Promise((resolve, reject) => {
 			pm2.list((err, list) => {
-				if (err) reject(err);
+				if (err) {
+					reject(err);
+				}
 				resolve(list);
 			});
 		});
 	}
 
-	async get({name}) {
+	async get(name) {
 		const list = await this.list();
 		return list.find(({name: pName}) => pName === name);
 	}
+}
+
+module.exports = {
+	PM2
 };
