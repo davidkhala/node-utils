6e8e1efcddb1c021b22111331b4e1582fee414ee
DavidLiu <david.yx.liu@oracle.com>
2022-04-23 22:16:42 +0800

    debug index.js compressing time variant
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

---
+++ compress/README.md
@@ -0,0 +1,3 @@
+# @davidkhala/compress
+
+- `fsExtra.copySync(src, dst)` will make the archive of `dst` time variant 
--- compress/test/index.js
+++ compress/test/index.js
@@ -5,7 +5,8 @@ import fs from 'fs';
 import compressing from 'compressing';
 import {sleep} from '@davidkhala/light';
 import assert from 'assert';
-import {create} from '../npm-tar.js'
+import {create} from '../npm-tar.js';
+import fsExtra from 'fs-extra';
 
 filedirname(import.meta);
 
@@ -16,10 +17,21 @@ describe('compress', function () {
 
 		const src = path.resolve(__dirname, 'fixture');
 		const dst = path.resolve(__dirname, 'fixture.tgz');
-		create(dst, src)
+		const srcCopy = path.resolve('src');
+		fsExtra.copySync(src, srcCopy, {preserveTimestamps: true});
+		create(dst, src);
 
 		const digest = sha2_256(fs.readFileSync(dst).toString());
-		console.debug(digest);
+
+
+		create(dst, srcCopy);
+		const digestCopy = sha2_256(fs.readFileSync(dst).toString());
+		console.debug({digestCopy});// 2a7724dfc54f02559e9909d1204ad9113b3c33bf928978ba6b5d5e987750bd25
+		fs.unlinkSync(dst);
+		await sleep(1000);
+		create(dst, src);
+		const digest2 = sha2_256(fs.readFileSync(dst).toString());
+		console.debug(digest === digest2);
 
 	});
 
