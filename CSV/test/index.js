dcff070a9fff2fcadd9d1e16f6f37114e63da7da
David Liu <david-khala@hotmail.com>
2025-01-13 16:50:42 +0800

    harmless array concat

--- CSV/test/index.js
+++ CSV/test/index.js
@@ -12,56 +12,59 @@ import papaParse from 'papaparse';
 filedirname(import.meta);
 describe('FromFile', () => {
 
-	it('dummy', () => {
-		const recoveredMatrix = FromFile(path.resolve(__dirname, 'dummy.csv'));
-		console.info(recoveredMatrix);
-	});
-	it('HRDataset_v14.csv', async () => {
-		const url = 'https://www.kaggle.com/api/v1/datasets/download/rhuebner/human-resources-data-set';
-		const zipDir = __dirname;
-		await download(url, zipDir);
+    it('dummy', () => {
+        const recoveredMatrix = FromFile(path.resolve(__dirname, 'dummy.csv'));
+        console.info(recoveredMatrix);
+    });
+    it('HRDataset_v14.csv', async () => {
+        const url = 'https://www.kaggle.com/api/v1/datasets/download/rhuebner/human-resources-data-set';
+        const zipDir = __dirname;
+        await download(url, zipDir);
 
-		const zipPath = path.resolve(zipDir, 'human-resources-data-set.zip');
-		const csvDir = __dirname;
-		await decompress(zipPath, csvDir, {plugins: [decompressUnzip()]});
+        const zipPath = path.resolve(zipDir, 'human-resources-data-set.zip');
+        const csvDir = __dirname;
+        await decompress(zipPath, csvDir, {plugins: [decompressUnzip()]});
 
-		const cvsPath = path.resolve(csvDir, 'HRDataset_v14.csv');
-		const recoveredMatrix = FromFile(cvsPath);
-		console.info(recoveredMatrix);
-		// cleanup
-		fs.unlinkSync(cvsPath);
-		fs.unlinkSync(zipPath);
-	});
+        const cvsPath = path.resolve(csvDir, 'HRDataset_v14.csv');
+        const recoveredMatrix = FromFile(cvsPath);
+        console.info(recoveredMatrix);
+        // cleanup
+        fs.unlinkSync(cvsPath);
+        fs.unlinkSync(zipPath);
+    });
 
 
 });
 
 describe('ToFile', () => {
-	it('inflateHeader', () => {
+    it('inflateHeader', () => {
 
-		const data = [
-			{a: 'b', c: 'd'},
-			{a: 'b', c: 'd', Column1: 'foo', Column2: 'bar',}
-		];
-		const data2 = _.cloneDeep(data);
-		const expected1 = `a,c,Column1,Column2
+        const data = [{a: 'b', c: 'd'}, {a: 'b', c: 'd', Column1: 'foo', Column2: 'bar',}, {
+            a: 'b',
+            c: 'd',
+            Column1: 'foo',
+            Column2: 'bar',
+        }];
+        const data2 = _.cloneDeep(data);
+        const expected1 = `a,c,Column1,Column2
 b,d,,
+b,d,foo,bar
 b,d,foo,bar`;
-		assert.strictEqual(ToFile(data, undefined, true), expected1);
+        assert.strictEqual(ToFile(data, undefined, true), expected1);
 
-		assert.strictEqual(ToFile(data2), 'a,c\nb,d\nb,d');
-	});
-	it('empty data array', () => {
-		const recoveredCSV = ToFile([]);
-		assert.strictEqual(recoveredCSV, '');
-		assert.strictEqual(papaParse.unparse([]), '');
-	});
-	it('undefined value', () => {
-		const data = [{a: null}];
-		const recoveredCSV = ToFile(data);
-		const expect1 = 'a\n';
-		assert.strictEqual(recoveredCSV, expect1);
-	});
+        assert.strictEqual(ToFile(data2), 'a,c\nb,d\nb,d\nb,d');
+    });
+    it('empty data array', () => {
+        const recoveredCSV = ToFile([]);
+        assert.strictEqual(recoveredCSV, '');
+        assert.strictEqual(papaParse.unparse([]), '');
+    });
+    it('undefined value', () => {
+        const data = [{a: null}];
+        const recoveredCSV = ToFile(data);
+        const expect1 = 'a\n';
+        assert.strictEqual(recoveredCSV, expect1);
+    });
 });
 
 
