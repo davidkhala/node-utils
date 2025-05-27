a7ecf71b55c24ed2cf64dc6aaade411459d846b3
David Liu <david-khala@hotmail.com>
2025-05-27 14:34:37 +0800

    @davidkhala/nodeutils: 0.1.1
    fix for express 5

--- main/baseApp.js
+++ main/baseApp.js
@@ -22,26 +22,26 @@ const minVersions = ['TLSv1', 'TLSv1.1', 'TLSv1.2'];
  * Default: none.
  */
 const secureProtocols = [
-	'TLS_method',
-	'TLS_client_method',
-	'TLS_server_method',
-	'TLSv1_2_method',
-	'TLSv1_2_client_method',
-	'TLSv1_2_server_method',
-	'TLSv1_1_method',
-	'TLSv1_1_client_method',
-	'TLSv1_1_server_method',
-	'TLSv1_method',
-	'TLSv1_client_method',
-	'TLSv1_server_method',
-	'SSLv3_method',
-	'SSLv3_client_method',
-	'SSLv3_server_method'
+    'TLS_method',
+    'TLS_client_method',
+    'TLS_server_method',
+    'TLSv1_2_method',
+    'TLSv1_2_client_method',
+    'TLSv1_2_server_method',
+    'TLSv1_1_method',
+    'TLSv1_1_client_method',
+    'TLSv1_1_server_method',
+    'TLSv1_method',
+    'TLSv1_client_method',
+    'TLSv1_server_method',
+    'SSLv3_method',
+    'SSLv3_client_method',
+    'SSLv3_server_method'
 ];
 
 export const httpsOptions = {
-	minVersion: minVersions,
-	secureProtocol: secureProtocols
+    minVersion: minVersions,
+    secureProtocol: secureProtocols
 };
 
 /**
@@ -56,72 +56,81 @@ export const httpsOptions = {
  * @returns {{app: Express, server: Server}}
  */
 export const run = (port, host, tlsOptions, logger = console) => {
-	const app = express();
-	app.options('*', cors());
-	app.use(cors());
-	app.use(bodyParser.json());
+    const app = express();
 
-	app.use(bodyParser.urlencoded({
-		extended: true// req.body will contain key-value pairs, extended=false: value can be a string or array; extended=true: value can be any type
-	}));
-	let server;
-	if (tlsOptions) {
-		const {key, cert, ca, requestCert} = tlsOptions;
+    if (!host) {
+        app.use(cors());
+    }
 
-		tlsOptions.key = isPath(key) ? fs.readFileSync(key) : key;
-		tlsOptions.cert = isPath(cert) ? fs.readFileSync(cert) : cert;
-		tlsOptions.ca = isPath(ca) ? fs.readFileSync(ca) : ca;
+    app.use(bodyParser.json());
 
-		server = https.createServer(tlsOptions, app).listen(port, host, () => {
-			logger.info('https server started at', {host, port, cert, ca, requestCert});
-		});
-	} else {
-		server = http.createServer(app).listen(port, host, () => {
-			logger.info('http server started at', {host, port});
-		});
-	}
+    app.use(bodyParser.urlencoded({
+        extended: true// req.body will contain key-value pairs, extended=false: value can be a string or array; extended=true: value can be any type
+    }));
+    let server;
+    if (tlsOptions) {
+        const {key, cert, ca, requestCert} = tlsOptions;
 
-	server.timeout = 240000;
-	return {app, server};
+        tlsOptions.key = isPath(key) ? fs.readFileSync(key) : key;
+        tlsOptions.cert = isPath(cert) ? fs.readFileSync(cert) : cert;
+        tlsOptions.ca = isPath(ca) ? fs.readFileSync(ca) : ca;
+
+        server = https.createServer(tlsOptions, app).listen(port, host, (error) => {
+            if (error) {
+                throw error
+            }
+            logger.info('https server started at', {host, port, cert, ca, requestCert});
+        });
+    } else {
+        server = http.createServer(app).listen(port, host, (error) => {
+            if (error) {
+                throw error
+            }
+            logger.info('http server started at', {host, port});
+        });
+    }
+
+    server.timeout = 240000;
+    return {app, server};
 };
 export const getRouter = () => {
-	return express.Router();
+    return express.Router();
 };
 export const expressError = (app, onError, logger = console) => {
-	if (!onError) {
-		onError = (err, res) => {
-			let status = 500;
-			if (err.statusCode) {
-				status = err.statusCode;
-			} else if (err.status) {
-				status = err.status;
-			}
-			res.status(status);
-			res.send(err);
-		};
-	}
-	app.use((err, req, res, next) => {
-		logger.error(err);
-		if (res.headersSent) {
-			logger.error(`error happened after res.headersSent=${res.headersSent}`);
-			return next(err);
-		}
-		onError(err, res);
-	});
+    if (!onError) {
+        onError = (err, res) => {
+            let status = 500;
+            if (err.statusCode) {
+                status = err.statusCode;
+            } else if (err.status) {
+                status = err.status;
+            }
+            res.status(status);
+            res.send(err);
+        };
+    }
+    app.use((err, req, res, next) => {
+        logger.error(err);
+        if (res.headersSent) {
+            logger.error(`error happened after res.headersSent=${res.headersSent}`);
+            return next(err);
+        }
+        onError(err, res);
+    });
 };
 export const formDataRouter = (cacheDir, fileSize) => {
-	const options = {
-		dest: cacheDir,
-		limits: {}
-	};
+    const options = {
+        dest: cacheDir,
+        limits: {}
+    };
 
-	if (Number.isInteger(fileSize) && fileSize > 0) {
-		options.limits.fileSize = fileSize; // Compliant value: 8000000
-	}
+    if (Number.isInteger(fileSize) && fileSize > 0) {
+        options.limits.fileSize = fileSize; // Compliant value: 8000000
+    }
 
-	const multerCache = Multer(options);
-	return multerCache.any();
+    const multerCache = Multer(options);
+    return multerCache.any();
 };
 export const formDataFilePaths = (req, property) => {
-	return req.files[property].map(({path}) => path);
+    return req.files[property].map(({path}) => path);
 };
---
+++ main/test/appTest.js
@@ -0,0 +1,25 @@
+import {run} from '../baseApp.js'
+
+describe('baseApp', function () {
+    this.timeout(0)
+    const start = (host) => {
+        const port = 13000
+        const {app, server} = run(port, host)
+
+        app.get('/', (req, res) => {
+            console.info(`\n from ${req.ip}`);
+            res.send('\npong\n');
+        });
+        if (process.env.CI) {
+            server.close()
+        }
+    }
+
+    it('run', async () => {
+        start()
+    })
+    it('run on localhost', async () => {
+        start('localhost')
+    })
+
+})
